f5932af075b173807073cf183af6bec3dee2d750
Ikeolu Kelechi Lucky <kelechiikeolu@gmail.com>
2025-02-02 05:21:47 +0100

    Feat: Added starter files for react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import About from "./components/about";
+import Signup from "./components/Signup";
+
+export default function App() {
+  return (
+   <div>
+  <About />
+  <Signup />
+   </div>
+  )
+}
